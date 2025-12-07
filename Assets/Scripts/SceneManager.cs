1c3648f3f92ca3b73e7ae8ebe40a49a633b3d2c8
Govind Choudhary <govindchoudhary844@gmail.com>
2025-12-07 10:25:00 +0530

    UI Update and Quit Button Added

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -21,5 +21,14 @@ public class SceneManager : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
     }
 
+    public void QuitGame()
+    {
+        // works in a built executable; editor play-mode stops below
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
 
 }
